ef3bfffceae0c765b66e98308494f53073923d4e
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2023-05-08 15:59:49 +0700

    2 files modified

--- src/app/components/multi-level-menu/multi-level-menu.module.ts
+++ src/app/components/multi-level-menu/multi-level-menu.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MultiLevelMenuComponent } from './multi-level-menu.component';
-import { DxPopoverModule, DxTemplateModule } from 'devextreme-angular';
 
 @NgModule({
   imports: [CommonModule, DxPopoverModule, DxTemplateModule],
